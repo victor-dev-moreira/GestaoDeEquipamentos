6960ace1e1d910a4981d1d0ac77784ec78d21883
Victor <victor_hugo_moreira@hotmail.com>
2026-05-12 21:25:13 -0300

    first commit

---
+++ GestaoDeEquipamentos.ConsoleApp/Dominio/Equipamento.cs
@@ -0,0 +1,18 @@
+namespace GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+/*
+
+• Deve mostrar o id;
+• Deve mostrar o nome;
+• Deve mostrar o preço;
+• Deve mostrar a fabricante;
+• Deve mostrar a data de fabricação;
+
+*/
+public class Equipamento
+{
+    public int id;
+    public string nome;
+    public decimal preco;
+    public DateTime dataFabricacao;
+}
---
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Dominio;
+
+DateTime dataAgora = DateTime.Now; // data de agora
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("Gestão de Equipamentos");
+    Console.WriteLine("---------------------------------");
+    Console.WriteLine("1 - Cadastrar equipamento");
+    Console.WriteLine("2 - Editar equipamento");
+    Console.WriteLine("3 - Excluir equipamento");
+    Console.WriteLine("4 - Visualizar equipamentos");
+    Console.WriteLine("S - Sair");
+    Console.WriteLine("---------------------------------");
+    Console.Write("> ");
+    string? opcaoMenu = Console.ReadLine()?.ToUpper();
+
+    if (opcaoMenu == "S")
+    {
+        Console.Clear();
+        break;
+    }
+
+    if (opcaoMenu == "1")
+    {
+    }
+    else if (opcaoMenu == "2")
+    {
+    }
+    else if (opcaoMenu == "3")
+    {
+    }
+    else if (opcaoMenu == "4")
+    {
+    }
+}
