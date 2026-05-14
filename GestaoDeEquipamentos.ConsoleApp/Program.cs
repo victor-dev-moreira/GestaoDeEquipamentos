97ac6da9fe1aeb4c317886f7bc68b41d8c6dc838
Victor <victor_hugo_moreira@hotmail.com>
2026-05-13 21:23:43 -0300

    Adicionado Opcão de editar, excluir e visualizar equipamentos

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -30,6 +30,7 @@ while (true)
         Console.WriteLine("---------------------------------");
         Console.WriteLine("Gestão de Equipamentos");
         Console.WriteLine("---------------------------------");
+
         Console.Write("Qual nome do equipamento? ");
         string nome = Console.ReadLine();
         Console.WriteLine("---------------------------------");
@@ -61,11 +62,120 @@ while (true)
     }
     else if (opcaoMenu == "2")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Edicão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preço de Aquisição", "Data de Fabricação"
+        );
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+        );
+        }
+        Console.Write("Qual SKU deseja editar? ");
+        int idEditar = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Qual nome do equipamento? ");
+        string nome = Console.ReadLine();
+        Console.WriteLine("---------------------------------");
+
+        Console.Write("Qual preco de aquisicão? ");
+        decimal precoAquisicao = decimal.Parse(Console.ReadLine());
+
+        Console.WriteLine("---------------------------------");
+        Console.Write("Digite a data de fabricação do equipamento: ");
+        DateTime dataFabricacao = DateTime.Parse(Console.ReadLine());
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento equipamentoEditado = equipamentosSalvos[i];
+
+            if (equipamentoEditado == null)
+                continue;
+
+            if (equipamentoEditado.id == idEditar)
+            {
+                equipamentoEditado.nome = nome;
+                equipamentoEditado.precoAquisicao = precoAquisicao;
+                equipamentoEditado.dataFabricacao = dataFabricacao;
+                break;
+            }
+        }
+        Console.WriteLine("Equipamento editado com sucesso!");
+        Console.ReadLine();
     }
     else if (opcaoMenu == "3")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Edicão de Equipamentos");
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preço de Aquisição", "Data de Fabricação"
+        );
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+        );
+        }
+
+        Console.Write("Qual Id do equipamento que deseja excluir? ");
+        int idExcluir = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento eq = equipamentosSalvos[i];
+            if (equipamentosSalvos[i] == null)
+                continue;
+
+            if (eq.id == idExcluir)
+            {
+                equipamentosSalvos[i] = null;
+                break;
+            }
+        }
+        Console.WriteLine("Equipamento excluido com sucesso!");
+        Console.ReadLine();
     }
     else if (opcaoMenu == "4")
     {
+        Console.WriteLine("---------------------------------");
+        Console.WriteLine("Visulizar Equipamentos");
+        Console.WriteLine("---------------------------------");
+
+        Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            "Id", "Nome", "Preço de Aquisição", "Data de Fabricação"
+        );
+        for (int i = 0; i < equipamentosSalvos.Length; i++)
+        {
+            Equipamento eq = equipamentosSalvos[i];
+
+            if (eq == null)
+                continue;
+
+            Console.WriteLine(
+            "{0, -7} | {1, -15} | {2, -20} | {3, -15}",
+            eq.id, eq.nome, eq.precoAquisicao, eq.dataFabricacao
+        );
+        }
+        Console.ReadLine();
     }
 }
--- GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
+++ GestaoDeEquipamentos.ConsoleApp/obj/Debug/net10.0/GestaoDeEquipamentos.ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6960ace1e1d910a4981d1d0ac77784ec78d21883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+579ed1a16f642796c29b9ccb6087528c27e5e48d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoDeEquipamentos.ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
